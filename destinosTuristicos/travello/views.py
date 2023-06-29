82ac71b611e12c05cef70566da93cee874cf3e72
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2023-06-29 17:14:13 -0500

    Passing Dynamic Data in HTML

--- destinosTuristicos/travello/views.py
+++ destinosTuristicos/travello/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()
+    dest1.name = "Arequipa"
+    dest1.desc = "La ciudad blanca"
+    dest1.img = "destination_1.jpg"
+    dest1.price = 50
+
+    dest2 = Destination()
+    dest2.name = "Tacna"
+    dest2.desc = "La ciudad heroica"
+    dest2.img = "destination_2.jpg"
+    dest2.price = 70
+
+    dest3 = Destination()
+    dest3.name = "Piura"
+    dest3.desc = "Hermosas playas"
+    dest3.img = "destination_3.jpg"
+    dest3.price = 90
+
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'dests': dests})
